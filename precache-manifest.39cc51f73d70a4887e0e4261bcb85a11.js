9a330b690b53ac7073ac128675b90bc26bb2f3a8
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-08-03 19:49:04 -0700

    Updates

---
+++ precache-manifest.39cc51f73d70a4887e0e4261bcb85a11.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb0be1b9e4d5e041ceef2032eb0c90b",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "96e0f79335090c872b02",
+    "url": "/portfolio2/static/css/main.0121a7f6.chunk.css"
+  },
+  {
+    "revision": "048bf87aea491761de35",
+    "url": "/portfolio2/static/js/2.10568890.chunk.js"
+  },
+  {
+    "revision": "19665a5136508df4d046bc8ef4481368",
+    "url": "/portfolio2/static/js/2.10568890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e0f79335090c872b02",
+    "url": "/portfolio2/static/js/main.c7b7175c.chunk.js"
+  },
+  {
+    "revision": "10a85a560f5bbed4544a",
+    "url": "/portfolio2/static/js/runtime-main.afe67eb9.js"
+  },
+  {
+    "revision": "d4306d875559469f2f34fe16b9b30804",
+    "url": "/portfolio2/static/media/aboutPic.d4306d87.jpg"
+  },
+  {
+    "revision": "c25c68aa135fb68c3e4390477f93a536",
+    "url": "/portfolio2/static/media/blur.c25c68aa.png"
+  },
+  {
+    "revision": "8e8efaa8308938309489a6c9d04ff653",
+    "url": "/portfolio2/static/media/fourcast.8e8efaa8.png"
+  },
+  {
+    "revision": "4c267a07b03d58a9711bf71a508bb786",
+    "url": "/portfolio2/static/media/image.4c267a07.jpg"
+  },
+  {
+    "revision": "d2339d8c64d892ed7364ff52314e0a01",
+    "url": "/portfolio2/static/media/logo.d2339d8c.png"
+  },
+  {
+    "revision": "df776ca640d17de2ecc9529772c60dce",
+    "url": "/portfolio2/static/media/weatherApp.df776ca6.png"
+  }
+]);
